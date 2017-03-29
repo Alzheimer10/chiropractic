1c82db70e608aff02edd60baa7f93c435d057a65
Alzhiemer10 <carlosanselmi2@gmail.com>
2017-03-29 14:46:43 -0400

    text

--- laravel/resources/views/complements/idiomaSelect.blade.php
+++ laravel/resources/views/complements/idiomaSelect.blade.php
@@ -1,10 +1,8 @@
-<div>
 <select name="lang" id="lang" class="selectpicker" onChange="window.location.href=this.value">
-
     <option   value="">{!! trans('header.main.lang') !!} </option>
 	@foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-
-    <option data-icon="flag {{$localeCode}}" value="{{LaravelLocalization::getLocalizedURL($localeCode) }}">{{ $properties['native'] }}</option>
+    	<option data-icon="flag {{$localeCode}}" value="{{LaravelLocalization::getLocalizedURL($localeCode) }}" 
+			@if(LaravelLocalization::getCurrentLocale() == $localeCode) selected @endif
+    	>{{ $properties['native'] }}</option>
 	@endforeach
 </select>	
-</div>
--- laravel/resources/views/pages/about/history.blade.php
+++ laravel/resources/views/pages/about/history.blade.php
@@ -5,7 +5,6 @@
 @section('content')
 {!! trans('header.main.submenu.about.2') !!}
 {!! trans('header.main.text.about2') !!}
-
 @stop
 
 @section('js')
